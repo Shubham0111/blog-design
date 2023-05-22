c3a4ba5567521e47fb209b262515673329da5657
Shubham <shubhamchandel567@gmail.com>
2023-05-22 11:58:37 +0530

    first commit

---
+++ src/helper/postlist.js
@@ -0,0 +1,44 @@
+import first from '../assets/b5.jpg'
+import second from '../assets/pexels-andrea-davis-3653849.jpg'
+import third from '../assets/pexels-elvis-2528118.jpg'
+import fourth from '../assets/pexels-jacob-colvin-1761279.jpg'
+import fifth from '../assets/pexels-pixabay-267389.jpg'
+
+export const postlist=[
+    {
+     tags:'Music,life',
+     name:'Lorem ipsum dolor sit amet',
+     time:'1 hour ago',
+      img:first,
+     text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime magni reprehenderit repellendus esse quidem possimus, reiciendis odio illo aperiam? Unde expedita nemo nihil eos!...'
+    },
+    {
+        tags:'Music,life',
+        name:'Lorem ipsum dolor sit amet',
+        time:'1 hour ago',
+        img:second,
+        text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime magni reprehenderit repellendus esse quidem possimus, reiciendis odio illo aperiam? Unde expedita nemo nihil eos!...'
+       },
+       {
+        tags:'Music,life',
+        name:'Lorem ipsum dolor sit amet',
+        time:'1 hour ago',
+        img:third,
+        text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime magni reprehenderit repellendus esse quidem possimus, reiciendis odio illo aperiam? Unde expedita nemo nihil eos!...'
+       },
+       {
+        tags:'Music,life',
+        name:'Lorem ipsum dolor sit amet',
+        time:'1 hour ago',
+        img:fourth,
+        text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime magni reprehenderit repellendus esse quidem possimus, reiciendis odio illo aperiam? Unde expedita nemo nihil eos!...'
+       },
+       {
+        tags:'Music,life',
+        name:'Lorem ipsum dolor sit amet',
+        time:'1 hour ago',
+        img:fifth,
+        text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime magni reprehenderit repellendus esse quidem possimus, reiciendis odio illo aperiam? Unde expedita nemo nihil eos!...'
+       }
+       
+]
